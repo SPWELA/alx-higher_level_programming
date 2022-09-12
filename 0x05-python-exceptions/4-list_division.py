eb650ab4917ba7a1813fe8b0728708efd1ba0d25
SPWELA <sindisiwemp@gmail.com>
2022-09-12 12:47:00 -0700

    sort pycode error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for num in range(0, list_length):
+    for i in range(list_length):
         try:
-            divide = my_list_1[num] / my_list_2[num]
-        except TypeError:
+            result = my_list_1[i] / my_list_2[i]
+        except (ValueError, TypeError):
             print("wrong type")
-            divide = 0
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            divide = 0
+            result = 0
         except IndexError:
             print("out of range")
-            divide = 0
+            result = 0
         finally:
-            new_list.append(divide)
-            return new_list
+            new_list.append(result)
+    return new_list
