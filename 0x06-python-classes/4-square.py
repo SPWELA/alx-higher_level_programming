0b966d74fb177d1b005c0b06b1695304bf528955
SPWELA <103654560+SPWELA@users.noreply.github.com>
2022-09-22 00:43:22 +0200

    update file 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,3 +22,9 @@ class Square():
 
     def area(self):
         return self.__size ** 2
+
+    def my_print(self):
+        if not self.__size:
+            print("")
+        for i in range(self.__size):
+            print("#" * self.__size)
